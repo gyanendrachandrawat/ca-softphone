d79a541506717bfb9b13d4ca636cf78a8664bfa0
karan-nishad <karan.n@consultadd.com>
2023-07-13 23:13:46 +0530

    added call logs

--- build-docker.sh
+++ build-docker.sh
@@ -2,5 +2,5 @@
 
 docker rm -f $(docker ps -a -q --filter="ancestor=twilio-softphone")
 docker build -t twilio-softphone .
-docker run -p 80:8080 -e ENV_NAME=dev -d -n softphone twilio-softphone
+docker run -p 80:8080 -e ENV_NAME=dev -d --name softphone twilio-softphone
 
