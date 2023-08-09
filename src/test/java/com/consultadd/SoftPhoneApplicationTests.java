fc66351919567822406cf5fa2cee6392bdd67dbf
karan-nishad <karan.n@consultadd.com>
2023-08-09 09:39:48 +0530

    Restored Test

--- src/test/java/com/consultadd/SoftPhoneApplicationTests.java
+++ src/test/java/com/consultadd/SoftPhoneApplicationTests.java
@@ -2,8 +2,12 @@ package com.consultadd;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
+@ActiveProfiles("test")
 class SoftPhoneApplicationTests {
 
     @Test
